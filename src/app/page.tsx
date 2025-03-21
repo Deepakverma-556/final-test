44f7a38001a6246e99d9e0de52a8be0fa718d0af
Deepak Verma <deepakradialcode@gmail.com>
2025-03-21 07:51:29 +0530

    remove line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import DressStyle from "@/components/DressStyle";
 import HappyCustomers from "@/components/HappyCustomers";
 import Hero from "@/components/Hero";
 import NewArrivals from "@/components/NewArrivals";
-import OurHappyCustomer from "@/components/OurHappyCustomer";
 import TopSelling from "@/components/TopSelling";
 
 export default function Home() {
